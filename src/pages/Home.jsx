7fe6c16df2651a35458bf7860fbac0496e3ea9b6
kareem1243 <kreemhishem9@gmail.com>
2025-07-04 14:56:06 +0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '30px' }}>
+      <h1>مرحبًا بك في متجر نرمين 🛍️</h1>
+      <p>تم حل مشكلة الصفحة الرئيسية ✅</p>
+    </div>
+  );
+}
