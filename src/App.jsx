7d4289a9de5bf70dbff10b35dc217e8bbc1e7368
kareem1243 <kreemhishem9@gmail.com>
2025-07-04 15:16:18 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
-// 🚀 App.jsx - واجهة التطبيق الرئيسية لإدارة الصفحات والتنقل
-
-import React, { useState } from 'react';
+import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Cart from './pages/Cart';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Checkout from './pages/Checkout';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
-  const [darkMode, setDarkMode] = useState(true);
-
   return (
-    <div className={darkMode ? "bg-black text-white min-h-screen" : "bg-white text-black min-h-screen"}>
-      <Router>
-        <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
