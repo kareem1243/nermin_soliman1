e59202c22bdce2dd39c80002bc6d5aa773d89981
kareem1243 <kreemhishem9@gmail.com>
2025-07-10 00:25:55 +0300

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+// src/index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
