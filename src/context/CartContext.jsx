2afe3659b2801cc8b1e482de25f22bfd39bdc3e0
kareem1243 <kreemhishem9@gmail.com>
2025-07-10 00:51:38 +0300

    Create CartContext.jsx

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, useState, useContext } from "react";
+
+const CartContext = createContext();
+
+export const useCart = () => useContext(CartContext);
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItems((prev) => [...prev, item]);
+  };
+
+  const removeFromCart = (id) => {
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  const getTotalPrice = () => {
+    return cartItems.reduce((total, item) => total + item.price * (item.quantity || 1), 0);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cartItems, addToCart, removeFromCart, clearCart, getTotalPrice }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
