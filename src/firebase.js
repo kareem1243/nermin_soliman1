2eca258a599283b76f8797d43237822a62741649
kareem1243 <kreemhishem9@gmail.com>
2025-07-10 00:23:57 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 // src/firebase.js
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -8,18 +7,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyDv3hEFN0ggaaCf20RuVVYhnndfy38OBbc",
   authDomain: "nermin-soliman-store.firebaseapp.com",
   projectId: "nermin-soliman-store",
-  storageBucket: "nermin-soliman-store.firebasestorage.app",
+  storageBucket: "nermin-soliman-store.appspot.com",
   messagingSenderId: "43639651722",
   appId: "1:43639651722:web:dfcdfd510d739a42c6dc73"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Authentication
-const auth = getAuth(app);
-
-// Initialize Firestore Database
-const db = getFirestore(app);
-
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
